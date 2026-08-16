63eaed5eb51698efc6fc21a452b8df36bb23b7ab
user <26336510@gmail.com>
2026-08-16 13:25:59 +0300

    oda popcorn

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
+from kivy.metrics import dp
 from kivymd.app import MDApp
 from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.screen import MDScreen
 from kivy import platform
 from kivy.core.window import Window
 
+FPS = 60
+
+BULLET_SPEED = dp(10)
+SHIP_SPEED = dp(10)
 
 class MainScreen(MDScreen):
-    ...
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
+        self.eventkeys = {}
+        self.cartide = []
+
+    def update(self): #todo
+        ...
+
+    def releaseKey(self, key): #todo
+        ...
+
+    def moveleft(self): #todo
+        ...
+
+    def moveRight(self):  # todo
+        ...
+
+    def shot(self): #todo
+        ...
 
 class GameScreen(MDScreen):
     ...
